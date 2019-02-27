dc32eb0b0b96c6873d518cae9749ca14f59544cc
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-28 00:39:33 +0700

    🐧 config

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Chao Xin'));
+
+app.listen(port, () => 
+  console.log(`Server is running on port ${port}`)
+);
